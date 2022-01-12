6262b456f9107975fa3fd12a5e66a3812b8f5d74
Doull666 <Doull666@outlook.com>
2022-01-12 09:53:34 +0800

    airflow demo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # airflow_learning
-airflow学习
+## 内容介绍：airflow代码开发内容
+
+
+## airflow执行时间设置
+1. 'start_date': 
+- datetime(2021,12,23)，开始时间设置为2021/12/23的零点
+- airflow.utils.dates.days_ago(1)，开始时间为当前时间的前一天
+2. schedule_interval="0 22 * * 5"，执行周期设置为每周五晚上10点整开始执行
+3. 具体执行时间：
+- 第一次执行，会马上进行调度的初始化
+- 第二次执行，会根据调度周期的时间执行
+
+
---
+++ dags/demo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import airflow
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from datetime import datetime,timedelta
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+default_args = {
+    'owner': 'Doull',
+    'depends_on_past': False,
+    # 'start_date': airflow.utils.dates.days_ago(1),
+    'start_date': datetime(2021,12,23),
+    'email': ['xxx@163.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+    'queue': 'bash_queue',
+}
+
+# -------------------------------------------------------------------------------
+# dag
+dag = DAG(
+    dag_id='demo',
+    default_args=default_args,
+    schedule_interval="0 22 * * 5",
+    description='data updating')
+
+# -------------------------------------------------------------------------------
+start = DummyOperator(
+    task_id='start',
+    dag=dag
+)
+
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag
+)
+
+# -------------------------------------------------------------------------------
+
+demo = SSHOperator(
+    task_id="demo",
+    ssh_conn_id="ssh_conn",
+    command="kinit -kt /tmp/supergroup.keytab supergroup;bash /home/demo.sh ",
+    dag=dag
+)
+
+
+start >> demo >> end
